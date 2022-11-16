763dd89b6b88f93b87e63ba1cc6f842945627c33
Adlet Kurentaev <kurentaev@gmail.com>
2022-11-16 14:57:05 +0600

    Сокращен код первого задания

--- source/apiapp/views.py
+++ source/apiapp/views.py
@@ -1,15 +1,42 @@
 import json
-
 from django.http import HttpResponse, HttpResponseNotAllowed, JsonResponse
 from django.views.decorators.csrf import ensure_csrf_cookie, csrf_exempt
 
 
-def is_number(str):
+def is_number(element):
     try:
-        float(str)
+        float(element)
         return True
     except ValueError:
         return False
+
+
+def calculator(request, operation):
+    if request.method == 'POST':
+        if request.body:
+            try:
+                answer = json.loads(request.body)
+                if is_number(answer['A']) and is_number(answer['B']):
+                    if operation == 'add':
+                        answer = float(answer['A']) + float(answer['B'])
+                    elif operation == 'subtract':
+                        answer = float(answer['A']) - float(answer['B'])
+                    elif operation == 'multiply':
+                        answer = float(answer['A']) * float(answer['B'])
+                    if operation == 'divide' and answer['B'] != 0:
+                        answer = float(answer['A']) / float(answer['B'])
+                    return JsonResponse({'answer': float(answer)}, safe=False)
+                response = JsonResponse({'error': 'Value error!'})
+                response.status_code = 400
+                return response
+            except:
+                response = JsonResponse({'error': 'Value error!'})
+                response.status_code = 400
+                return response
+        else:
+            response = JsonResponse({'error': 'No data provided!'})
+            response.status_code = 400
+            return response
 
 
 @ensure_csrf_cookie
@@ -21,87 +48,19 @@ def get_token_view(request, *args, **kwargs):
 
 @csrf_exempt
 def add_view(request, *args, **kwargs):
-    if request.method == 'POST':
-        if request.body:
-            try:
-                answer = json.loads(request.body)
-                if is_number(answer['A']) and is_number(answer['B']):
-                    answer = float(answer['A']) + float(answer['B'])
-                    return JsonResponse({'answer': float(answer)}, safe=False)
-                response = JsonResponse({'error': 'Value error!'})
-                response.status_code = 400
-                return response
-            except:
-                response = JsonResponse({'error': 'Value error!'})
-                response.status_code = 400
-                return response
-        else:
-            response = JsonResponse({'error': 'No data provided!'})
-            response.status_code = 400
-            return response
+    return calculator(request, 'add')
 
 
 @csrf_exempt
 def subtract_view(request, *args, **kwargs):
-    if request.method == 'POST':
-        if request.body:
-            try:
-                answer = json.loads(request.body)
-                if is_number(answer['A']) and is_number(answer['B']):
-                    answer = float(answer['A']) - float(answer['B'])
-                    return JsonResponse({'answer': float(answer)}, safe=False)
-                response = JsonResponse({'error': 'Value error!'})
-                response.status_code = 400
-                return response
-            except:
-                response = JsonResponse({'error': 'Value error!'})
-                response.status_code = 400
-                return response
-        else:
-            response = JsonResponse({'error': 'No data provided!'})
-            response.status_code = 400
-            return response
+    return calculator(request, 'subtract')
 
 
 @csrf_exempt
 def multiply_view(request, *args, **kwargs):
-    if request.method == 'POST':
-        if request.body:
-            try:
-                answer = json.loads(request.body)
-                if is_number(answer['A']) and is_number(answer['B']):
-                    answer = float(answer['A']) * float(answer['B'])
-                    return JsonResponse({'answer': float(answer)}, safe=False)
-                response = JsonResponse({'error': 'Value error!'})
-                response.status_code = 400
-                return response
-            except:
-                response = JsonResponse({'error': 'Value error!'})
-                response.status_code = 400
-                return response
-        else:
-            response = JsonResponse({'error': 'No data provided!'})
-            response.status_code = 400
-            return response
+    return calculator(request, 'multiply')
 
 
 @csrf_exempt
 def divide_view(request, *args, **kwargs):
-    if request.method == 'POST':
-        if request.body:
-            try:
-                answer = json.loads(request.body)
-                if is_number(answer['A']) and is_number(answer['B']) and answer['B'] != 0:
-                    answer = float(answer['A']) + float(answer['B'])
-                    return JsonResponse({'answer': float(answer)}, safe=False)
-                response = JsonResponse({'error': 'Value error!'})
-                response.status_code = 400
-                return response
-            except:
-                response = JsonResponse({'error': 'Value error!'})
-                response.status_code = 400
-                return response
-        else:
-            response = JsonResponse({'error': 'No data provided!'})
-            response.status_code = 400
-            return response
+    return calculator(request, 'divide')
